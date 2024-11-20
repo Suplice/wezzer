f3de62cdbfddb806d6a048f49c9431dc8bf1642b
Mateusz Duma <mateuszsuplice@gmail.com>
2024-11-20 20:50:43 +0100

    refactor: Update MainField component structure for improved readability and maintainability

--- frontend/src/Components/MainField/MainField.tsx
+++ frontend/src/Components/MainField/MainField.tsx
@@ -15,24 +15,26 @@ const MainField: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col max-w-[1600px] mx-auto mt-5 h-full min-h-[calc(100vh-92px)] no-scroll ">
-      <div className="flex flex-row justify-between px-6  ">
-        <p className="font-semibold text-2xl">Browse Rooms</p>
-        <button
-          onClick={() => setIsFormVisible(true)}
-          className="bg-green-600 rounded-full text-white pr-1 pl-1 sm:pr-3  py-1 flex flex-row gap-3 items-center justify-center hover:bg-green-500 transition-all"
-        >
-          <p className="rounded-full text-green-600 bg-white p-2 text-center  text-md font-semibold ">
-            <FaPlus />
-          </p>
-          <p className="text-lg font-semibold hidden sm:flex">Create Room</p>
-        </button>
+    <>
+      <div className="flex flex-col max-w-[1600px] mx-auto mt-5 h-full min-h-[calc(100vh-92px)] no-scroll ">
+        <div className="flex flex-row justify-between px-6  ">
+          <p className="font-semibold text-2xl">Browse Rooms</p>
+          <button
+            onClick={() => setIsFormVisible(true)}
+            className="bg-green-600 rounded-full text-white pr-1 pl-1 sm:pr-3  py-1 flex flex-row gap-3 items-center justify-center hover:bg-green-500 transition-all"
+          >
+            <p className="rounded-full text-green-600 bg-white p-2 text-center  text-md font-semibold ">
+              <FaPlus />
+            </p>
+            <p className="text-lg font-semibold hidden sm:flex">Create Room</p>
+          </button>
+        </div>
+        <RoomManager />
       </div>
-      <RoomManager />
       <AnimatePresence>
         {isFormVisible && <AddRoomForm onClose={closeForm} />}
       </AnimatePresence>
-    </div>
+    </>
   );
 };
 
