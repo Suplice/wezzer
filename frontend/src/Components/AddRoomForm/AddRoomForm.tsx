669367bcb13474c901f89ab3dc46b961689b427d
Mateusz Duma <mateuszsuplice@gmail.com>
2024-11-20 21:19:01 +0100

    feat: Implement drag-and-drop functionality for image upload in AddRoomForm

--- frontend/src/Components/AddRoomForm/AddRoomForm.tsx
+++ frontend/src/Components/AddRoomForm/AddRoomForm.tsx
@@ -1,14 +1,54 @@
 import "./AddRoomForm.css";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
 import { FaImages } from "react-icons/fa";
+import { CgPlayListRemove } from "react-icons/cg";
+
 interface AddRoomFormProps {
   onClose: () => void;
 }
 
 const AddRoomForm: React.FC<AddRoomFormProps> = ({ onClose }) => {
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const fileDivInputRef = useRef<HTMLDivElement>(null);
+  const [dragStatus, setDragStatus] = useState<"default" | "valid" | "invalid">(
+    "default"
+  );
   const [selectedImage, setSelectedImage] = useState<string>("");
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  useEffect(() => {
+    document.addEventListener("dragover", (e) => e.preventDefault());
+    document.addEventListener("drop", (e) => e.preventDefault());
+  }, []);
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+    const file = e.dataTransfer.items[0];
+    console.log(file);
+    if (file) {
+      const isValid = file.type === "image/jpeg" || file.type === "image/jpg";
+      setDragStatus(isValid ? "valid" : "invalid");
+    }
+  };
+
+  const handleDragLeave = () => {
+    setDragStatus("default");
+  };
+
+  const handleDrop = (e: React.DragEvent) => {
+    e.preventDefault();
+    const file = e.dataTransfer.items[0]?.getAsFile();
+    if (file) {
+      const isValid = file.type === "image/jpeg" || file.type === "image/jpg";
+      setDragStatus("default");
+
+      if (isValid) {
+        setSelectedImage(file.name);
+        setSelectedFile(file);
+      }
+    }
+  };
 
   return (
     <div
@@ -19,14 +59,14 @@ const AddRoomForm: React.FC<AddRoomFormProps> = ({ onClose }) => {
         initial={{ y: 50, opacity: 0 }}
         animate={{ y: 0, opacity: 1, transition: { duration: 0.3 } }}
         exit={{ y: -50, opacity: 0, transition: { duration: 0.3 } }}
-        className="bg-white rounded-lg p-6  sm:w-1/2 lg:w-1/3 shadow-lg    "
+        className="bg-white rounded-lg p-6 sm:w-1/2 lg:w-1/3 shadow-lg"
         onClick={(e) => e.stopPropagation()}
       >
-        <div className="flex flex-row  justify-between mb-4 items-center">
-          <h2 className="text-xl font-bold  text-center ">Create Room</h2>
+        <div className="flex flex-row justify-between mb-4 items-center">
+          <h2 className="text-xl font-bold text-center">Create Room</h2>
           <button
             onClick={onClose}
-            className=" text-gray-400 hover:text-gray-600 text-xl pb-3"
+            className="text-gray-400 hover:text-gray-600 text-xl pb-3"
           >
             x
           </button>
@@ -34,7 +74,7 @@ const AddRoomForm: React.FC<AddRoomFormProps> = ({ onClose }) => {
 
         <form>
           <div className="mb-4">
-            <label className=" text-sm font-medium">Room Name</label>
+            <label className="text-sm font-medium">Room Name</label>
             <input
               type="text"
               id="roomName"
@@ -42,14 +82,23 @@ const AddRoomForm: React.FC<AddRoomFormProps> = ({ onClose }) => {
             />
           </div>
           <div className="mb-4">
-            <label className=" text-sm font-medium">Room Description</label>
+            <label className="text-sm font-medium">Room Description</label>
             <textarea
-              className="border w-full  outline-none p-2 resize-none border-gray-300  focus:ring-blue-500 focus:ring-2 rounded"
+              className="border w-full outline-none p-2 resize-none border-gray-300 focus:ring-blue-500 focus:ring-2 rounded"
               cols={10}
               rows={3}
             ></textarea>
           </div>
-          <div className="flex items-center justify-center w-full h-[200px] flex-col  mb-2 hover:cursor-pointer custom-dashed-border hover:bg-gray-100 border-gray-400 gap-4">
+          <div
+            className={`flex items-center justify-center w-full h-[200px] flex-col mb-2 hover:cursor-pointer custom-dashed-border transition-all relative gap-4 ${
+              dragStatus === "valid"
+                ? "bg-blue-100 border-blue-500 text-blue-600"
+                : dragStatus === "invalid"
+                ? "bg-red-100 border-red-500 text-red-600"
+                : "border-gray-400 hover:bg-gray-100"
+            }`}
+            ref={fileDivInputRef}
+          >
             <FaImages size={38} />
             <label className="text-xl font-semibold">
               Drag images here or select new files
@@ -60,10 +109,26 @@ const AddRoomForm: React.FC<AddRoomFormProps> = ({ onClose }) => {
               accept=".jpg, .jpeg"
               className="w-full hidden"
             />
+            <div
+              className="absolute w-full h-full"
+              onDragLeave={handleDragLeave}
+              onDrop={handleDrop}
+              onDragEnter={handleDragOver}
+            ></div>
           </div>
-          <div className="flex flex-row items-center  mb-2">
-            <p className=" text-lg font-semibold">Selected Image: </p>
+          <div className="flex flex-row items-center mb-2">
+            <p className="text-lg font-semibold">Selected Image: </p>
             <p className="text-lg ml-2"> {selectedImage}</p>
+            <CgPlayListRemove
+              onClick={() => {
+                setSelectedImage("");
+                setSelectedFile(null);
+              }}
+              size={24}
+              className={`pt-2 ml-1 hover:cursor-pointer text-red-600 ${
+                selectedImage ? "" : "hidden"
+              }`}
+            />
           </div>
           <div className="flex justify-end gap-2">
             <button
