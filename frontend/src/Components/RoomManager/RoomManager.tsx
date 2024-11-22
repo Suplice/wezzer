5bbed868a9624d554b9b18de8d5051ebd3e61267
Mateusz Duma <mateuszsuplice@gmail.com>
2024-11-23 00:02:06 +0100

    feat: Implement dynamic room loading in RoomManager component

--- frontend/src/Components/RoomManager/RoomManager.tsx
+++ frontend/src/Components/RoomManager/RoomManager.tsx
@@ -1,73 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RoomCard from "../RoomCard/RoomCard";
+import { Room } from "../../utils/models";
+import RoomsLoading from "../RoomsLoading/RoomsLoading";
 
 const RoomManager: React.FC = () => {
+  const [rooms, setRooms] = useState<Room[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getRooms = async () => {
+      setLoading(true);
+      const response = await fetch("http://localhost:8000/api/rooms", {});
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setRooms(data);
+        console.log(rooms);
+        setLoading(false);
+      }
+    };
+
+    getRooms();
+  }, []);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 p-5 ">
-      <RoomCard
-        roomName={"dadada"}
-        roomDescription={""}
-        roomBackground={""}
-        roomCreator={""}
-        roomCreaterAvatar={""}
-        roomId={"1"}
-      />
-      <RoomCard
-        roomName={""}
-        roomDescription={""}
-        roomBackground={""}
-        roomCreator={""}
-        roomCreaterAvatar={""}
-        roomId={"2"}
-      />
-      <RoomCard
-        roomName={""}
-        roomDescription={""}
-        roomBackground={""}
-        roomCreator={""}
-        roomCreaterAvatar={""}
-        roomId={"3"}
-      />
-      <RoomCard
-        roomName={""}
-        roomDescription={""}
-        roomBackground={""}
-        roomCreator={""}
-        roomCreaterAvatar={""}
-        roomId={"4"}
-      />
-      <RoomCard
-        roomName={""}
-        roomDescription={""}
-        roomBackground={""}
-        roomCreator={""}
-        roomCreaterAvatar={""}
-        roomId={"5"}
-      />
-      <RoomCard
-        roomName={""}
-        roomDescription={""}
-        roomBackground={""}
-        roomCreator={""}
-        roomCreaterAvatar={""}
-        roomId={"6"}
-      />
-      <RoomCard
-        roomName={""}
-        roomDescription={""}
-        roomBackground={""}
-        roomCreator={""}
-        roomCreaterAvatar={""}
-        roomId={"7"}
-      />
-      <RoomCard
-        roomName={""}
-        roomDescription={""}
-        roomBackground={""}
-        roomCreator={""}
-        roomCreaterAvatar={""}
-        roomId={"8"}
-      />
+      {loading && <RoomsLoading />}
+      {rooms.map((room) => (
+        <RoomCard
+          roomName={room.Name}
+          roomDescription={room.Description}
+          roomBackground={room.backgroundImage}
+          roomCreator={room.CreatorId}
+          roomCreaterAvatar={""}
+          roomId={room.RoomId}
+        />
+      ))}
     </div>
   );
 };
