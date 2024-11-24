42095a85d4b487b132a716ff4045b908179e1ef5
Mateusz Duma <mateuszsuplice@gmail.com>
2024-11-24 22:04:26 +0100

    feat: Update RoomsLoading component to display two skeletons for improved loading state

--- frontend/src/Components/RoomsLoading/RoomsLoading.tsx
+++ frontend/src/Components/RoomsLoading/RoomsLoading.tsx
@@ -16,7 +16,7 @@ const RoomsLoading: React.FC = () => {
             height={40}
           />
           <Skeleton count={1} baseColor="#888888" width="90%" />
-          <Skeleton count={1} baseColor="#888888" width="70%" />
+          <Skeleton count={2} baseColor="#888888" width="70%" />
           <Skeleton count={1} baseColor="#888888" width="50%" />
           <Skeleton count={1} baseColor="#888888" width="80%" />
           <Skeleton count={1} baseColor="#888888" width="40%" />
