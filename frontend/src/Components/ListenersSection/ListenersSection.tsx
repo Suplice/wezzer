cbd85cd86775d722740352a7a521b3f75266f70b
suplice <mateuszsuplice@gmail.com>
2024-11-27 13:31:54 +0100

    feat: Implemented ListenerUser component and used it in ListenersSection

--- frontend/src/Components/ListenersSection/ListenersSection.tsx
+++ frontend/src/Components/ListenersSection/ListenersSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ListenerUser from '../ListenerUser/ListenerUser';
 
 
 const ListenersSection: React.FC = () => {
@@ -6,35 +7,20 @@ const ListenersSection: React.FC = () => {
 
     return (
         <div className="w-full h-1/5 overflow-auto" >
-        <div className="flex flex-col items-center bg-orange-200 rounded-b-3xl shadow-lg shadow-zinc-500 ">
+        <div className="flex flex-col items-center bg-orange-200 rounded-b-3xl shadow-[0_8px_16px_3px_rgba(50,50,50,0.34)]  ">
             <div>
                 Listeners section
             </div>
             
             <div className="flex flex-row flex-wrap px-5 py-5 gap-10 overflow-hidden justify-center">
-                <div className="flex flex-col items-center justify-center">
-                    <img className="w-[60px] h-[60px] rounded-full bg-slate-400 border shadow-md shadow-slate-500 border-stone-300" src="/public/basicAvatar.png"></img>
-                    <p>User Name</p>
-                </div>
-                <div className="flex flex-col items-center justify-center">
-                    <img className="w-[60px] h-[60px] rounded-full bg-slate-400" src="/public/basicAvatar.png"></img>
-                    <p>User Name</p>
-                </div>
-                
-                <div className="flex flex-col items-center justify-center">
-                    <img className="w-[60px] h-[60px] rounded-full bg-slate-400" src="/public/basicAvatar.png"></img>
-                    <p>User Name</p>
-                </div>
-                
-                <div className="flex flex-col items-center justify-center">
-                    <img className="w-[60px] h-[60px] rounded-full bg-slate-400" src="/public/basicAvatar.png"></img>
-                    <p>User Name</p>
-                </div>
-                
-                <div className="flex flex-col items-center justify-center">
-                    <img className="w-[60px] h-[60px] rounded-full bg-slate-400" src="/public/basicAvatar.png"></img>
-                    <p>User Name</p>
-                </div>
+               <ListenerUser name="User 1" profileImg="/public/basicAvatar.png" />
+               <ListenerUser name="User 2" profileImg="/public/basicAvatar.png" />
+               <ListenerUser name="User 3" profileImg="/public/basicAvatar.png" />
+               <ListenerUser name="User 4" profileImg="/public/basicAvatar.png" />
+               <ListenerUser name="User 5" profileImg="/public/basicAvatar.png" />
+               <ListenerUser name="User 6" profileImg="/public/basicAvatar.png" />
+               <ListenerUser name="User 7" profileImg="/public/basicAvatar.png" />
+               
                 
             </div>
         </div>
