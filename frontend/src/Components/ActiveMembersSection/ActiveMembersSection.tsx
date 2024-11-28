2bfe38c98455d10a82d62f240480cb793377b29b
Mateusz Duma <mateuszsuplice@gmail.com>
2024-11-28 22:17:01 +0100

    feat: Enhance ActiveMembersSection with backdrop blur and improved layout

--- frontend/src/Components/ActiveMembersSection/ActiveMembersSection.tsx
+++ frontend/src/Components/ActiveMembersSection/ActiveMembersSection.tsx
@@ -1,28 +1,55 @@
-import React from 'react'
-import ActiveUser from '../ActiveUser/ActiveUser';
+import React from "react";
+import ActiveUser from "../ActiveUser/ActiveUser";
 
 const ActiveMembersSection: React.FC = () => {
-    return (
-        <div className="w-full h-4/5 overflow-auto ">
-            <div className="flex flex-col items-center">
-                <div>
-                    Active members section
-                </div>
-                <div className="flex flex-row flex-wrap px-5 py-5 gap-6 md:gap-x-12 gap-y-6 overflow-hidden items-center justify-center">
-                       <ActiveUser name="user 1" isActive={false} profileImg="/public/basicAvatar.png" />
-                       <ActiveUser name="user 1" isActive={false} profileImg="/public/basicAvatar.png" />
-                       <ActiveUser name="user 1" isActive={false} profileImg="/public/basicAvatar.png" />
-                       <ActiveUser name="user 1" isActive={false} profileImg="/public/basicAvatar.png" />
-                       <ActiveUser name="user 1" isActive={false} profileImg="/public/basicAvatar.png" />
-                       <ActiveUser name="user 1" isActive={false} profileImg="/public/basicAvatar.png" />
-                       <ActiveUser name="user 1" isActive={false} profileImg="/public/basicAvatar.png" />
-                       <ActiveUser name="user 1" isActive={false} profileImg="/public/basicAvatar.png" />
-                    
-                    
-                  </div>
-             </div>
+  return (
+    <div className="w-full h-4/5 overflow-auto backdrop-blur-xl ">
+      <div className="flex flex-col items-center">
+        <div className="flex flex-row flex-wrap px-5 py-5 gap-6 md:gap-x-12 gap-y-6 overflow-hidden items-center justify-center">
+          <ActiveUser
+            name="user 1"
+            isActive={false}
+            profileImg="/public/basicAvatar.png"
+          />
+          <ActiveUser
+            name="user 1"
+            isActive={false}
+            profileImg="/public/basicAvatar.png"
+          />
+          <ActiveUser
+            name="user 1"
+            isActive={false}
+            profileImg="/public/basicAvatar.png"
+          />
+          <ActiveUser
+            name="user 1"
+            isActive={false}
+            profileImg="/public/basicAvatar.png"
+          />
+          <ActiveUser
+            name="user 1"
+            isActive={false}
+            profileImg="/public/basicAvatar.png"
+          />
+          <ActiveUser
+            name="user 1"
+            isActive={false}
+            profileImg="/public/basicAvatar.png"
+          />
+          <ActiveUser
+            name="user 1"
+            isActive={false}
+            profileImg="/public/basicAvatar.png"
+          />
+          <ActiveUser
+            name="user 1"
+            isActive={false}
+            profileImg="/public/basicAvatar.png"
+          />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default ActiveMembersSection;
