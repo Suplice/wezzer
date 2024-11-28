386061f5b729e14851e4d24f44b9eed1cdb83217
Mateusz Duma <mateuszsuplice@gmail.com>
2024-11-28 22:17:09 +0100

    feat: Refactor ActiveUser component to improve type definitions and code structure

--- frontend/src/Components/ActiveUser/ActiveUser.tsx
+++ frontend/src/Components/ActiveUser/ActiveUser.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
-import { ListenerUserInterface } from '../ListenerUser/ListenerUser';
+import React from "react";
 
-interface ActiveUserInterface extends ListenerUserInterface{
-    isActive: boolean;
-} 
-
-
-const ActiveUser: React.FC<ActiveUserInterface> = ({name, isActive, profileImg}) => {
-
-    return (
-    <div className="flex flex-col items-center justify-center gap-1">
-        <img className={`w-[100px] h-[100px] rounded-full bg-slate-400 border shadow-md shadow-slate-500  border-stone-300 transition-all duration-75  ${isActive ? "outline-green-600 outline-4 outline " : "bg-slate-400 "}`} src={profileImg}></img>
-        <p>{name}</p>
-    </div>)
+interface ActiveUserInterface {
+  isActive: boolean;
+  name: string;
+  profileImg: string;
 }
+
+const ActiveUser: React.FC<ActiveUserInterface> = ({
+  name,
+  isActive,
+  profileImg,
+}) => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-1">
+      <img
+        className={`w-[100px] h-[100px] rounded-full bg-slate-400 border shadow-md shadow-slate-500  border-stone-300 transition-all duration-75  ${
+          isActive ? "outline-green-600 outline-4 outline " : "bg-slate-400 "
+        }`}
+        src={profileImg}
+      ></img>
+      <p>{name}</p>
+    </div>
+  );
+};
 
 export default ActiveUser;
