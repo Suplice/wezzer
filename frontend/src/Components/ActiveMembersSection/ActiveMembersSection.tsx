4ddc45f45bc32b2844f3f28577f38853cd4f314b
Mateusz Duma <mateuszsuplice@gmail.com>
2024-11-28 23:27:56 +0100

    feat: Add loading indicators to ActiveMembersSection and integrate react-loading-indicators

--- frontend/src/Components/ActiveMembersSection/ActiveMembersSection.tsx
+++ frontend/src/Components/ActiveMembersSection/ActiveMembersSection.tsx
@@ -1,53 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ActiveUser from "../ActiveUser/ActiveUser";
+import { BlinkBlur } from "react-loading-indicators";
 
 const ActiveMembersSection: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  useEffect(() => {
+    const connectToRoom = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/active-users");
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+        }
+
+        console.log("done");
+      } catch (e) {
+        console.log(e);
+        setIsLoading(false);
+      }
+    };
+
+    connectToRoom();
+  }, []);
+
   return (
-    <div className="w-full h-4/5 overflow-auto backdrop-blur-xl ">
-      <div className="flex flex-col items-center">
-        <div className="flex flex-row flex-wrap px-5 py-5 gap-6 md:gap-x-12 gap-y-6 overflow-hidden items-center justify-center">
-          <ActiveUser
-            name="user 1"
-            isActive={false}
-            profileImg="/public/basicAvatar.png"
-          />
-          <ActiveUser
-            name="user 1"
-            isActive={false}
-            profileImg="/public/basicAvatar.png"
-          />
-          <ActiveUser
-            name="user 1"
-            isActive={false}
-            profileImg="/public/basicAvatar.png"
-          />
-          <ActiveUser
-            name="user 1"
-            isActive={false}
-            profileImg="/public/basicAvatar.png"
-          />
-          <ActiveUser
-            name="user 1"
-            isActive={false}
-            profileImg="/public/basicAvatar.png"
-          />
-          <ActiveUser
-            name="user 1"
-            isActive={false}
-            profileImg="/public/basicAvatar.png"
-          />
-          <ActiveUser
-            name="user 1"
-            isActive={false}
-            profileImg="/public/basicAvatar.png"
-          />
-          <ActiveUser
-            name="user 1"
-            isActive={false}
-            profileImg="/public/basicAvatar.png"
-          />
+    <div className="h-full overflow-auto backdrop-blur-xl w-full backdrop-brightness-50   ">
+      {isLoading ? (
+        <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <BlinkBlur color="black" size="large" text="Loading" textColor="" />
         </div>
-      </div>
+      ) : (
+        <div className="flex flex-col items-center ">
+          <div className="flex flex-row flex-wrap px-5 py-5 gap-6 md:gap-x-12 gap-y-6 overflow-hidden items-center justify-center ">
+            <ActiveUser
+              name="user 1"
+              isActive={false}
+              profileImg="/public/basicAvatar.png"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
