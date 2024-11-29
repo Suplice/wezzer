189cc69a9ec938157aa9be56c5a6ee5e24f1a1f7
Mateusz Duma <mateuszsuplice@gmail.com>
2024-11-30 00:49:29 +0100

    feat: Update Navbar component to display user avatar when authenticated; fallback to login link when not authenticated

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useAuth } from "../../Context/AuthContext";
+import { Avatar } from "@mantine/core";
 
 const Navbar: React.FC = () => {
+  const { isAuthenticated, user } = useAuth();
+
   return (
     <div className="bg-sky-600 h-[72px] w-full md:px-8 px-4 sticky z-10 m-0 top-0">
       <div className=" h-[72px] flex flex-row justify-between mx-auto max-w-7xl items-center ">
@@ -17,12 +21,22 @@ const Navbar: React.FC = () => {
           <p className="font-bold text-white text-2xl">Wezzer</p>
         </div>
         <div className="flex">
-          <a
-            href="/login"
-            className="bg-white  text-xs md:text-base text-black px-5 md:px-8 py-2 font-bold rounded md:rounded-md  transition-all hover:bg-gray-200"
-          >
-            Login
-          </a>
+          {isAuthenticated ? (
+            <Avatar
+              variant="filled"
+              radius="xl"
+              size={45}
+              color="violet"
+              name={user?.name}
+            ></Avatar>
+          ) : (
+            <a
+              href="/login"
+              className="bg-white  text-xs md:text-base text-black px-5 md:px-8 py-2 font-bold rounded md:rounded-md  transition-all hover:bg-gray-200"
+            >
+              Login
+            </a>
+          )}
         </div>
       </div>
     </div>
