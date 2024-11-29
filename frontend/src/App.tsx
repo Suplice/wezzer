58ce94b77b13dead0facd8ff67b8709ffa875062
Mateusz Duma <mateuszsuplice@gmail.com>
2024-11-30 00:49:47 +0100

    feat: Wrap App component with MantineProvider for enhanced theming and styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,17 @@ import "./App.css";
 import { AuthProvider } from "./Context/AuthContext";
 import { ToastContainer } from "react-toastify";
 import Layout from "./Components/Layout/Layout";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 
 function App() {
   return (
-    <AuthProvider>
-      <Layout />
-      <ToastContainer />
-    </AuthProvider>
+    <MantineProvider>
+      <AuthProvider>
+        <Layout />
+        <ToastContainer />
+      </AuthProvider>
+    </MantineProvider>
   );
 }
 
