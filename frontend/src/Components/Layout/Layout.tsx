fc3d665351b307a6d9b5e48d601892b153d9ff99
Mateusz Duma <mateuszsuplice@gmail.com>
2024-11-30 00:00:10 +0100

    feat: Implement routing in Layout component; set up routes for Home, SignIn, SignUp, and Room pages with a 404 fallback

--- frontend/src/Components/Layout/Layout.tsx
+++ frontend/src/Components/Layout/Layout.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "../../Pages/Home/Home";
+import Room from "../../Pages/Room/Room";
+import SignIn from "../SignIn/SignIn";
+import SignUp from "../SignUp/SignUp";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/login",
+    element: <SignIn />,
+  },
+  {
+    path: "/Home",
+    element: <Home />,
+  },
+  {
+    // TODO 404 page
+    path: "*",
+    element: <div>404</div>,
+  },
+  {
+    path: "/r/:roomId",
+    element: <Room />,
+  },
+]);
 
 const Layout: React.FC = ({}) => {
-  return <div>Main</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default Layout;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,46 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App.tsx";
-import SignUp from "./Components/SignUp/SignUp.tsx";
-import SignIn from "./Components/SignIn/SignIn.tsx";
-import Home from "./Pages/Home/Home.tsx";
-import Room from "./Pages/Room/Room.tsx";
+import "react-toastify/dist/ReactToastify.css";
 import { AuthProvider } from "./Context/AuthContext.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/signup",
-    element: <SignUp />,
-  },
-  {
-    path: "/login",
-    element: <SignIn />,
-  },
-  {
-    path: "/Home",
-    element: <Home />,
-  },
-  {
-    // TODO 404 page
-    path: "*",
-    element: <div>404</div>,
-  },
-  {
-    path: "/r/:roomId",
-    element: <Room />,
-  },
-]);
+import { ToastContainer } from "react-toastify";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <App />
   </StrictMode>
 );
