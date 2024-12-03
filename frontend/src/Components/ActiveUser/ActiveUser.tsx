67d677e7be916025db45f6e8a40dbcdc79b5864a
Mateusz Duma <mateuszsuplice@gmail.com>
2024-12-03 23:11:11 +0100

    feat: add tooltip to display user name on hover in ActiveUser component

--- frontend/src/Components/ActiveUser/ActiveUser.tsx
+++ frontend/src/Components/ActiveUser/ActiveUser.tsx
@@ -1,4 +1,4 @@
-import { Avatar } from "@mantine/core";
+import { Avatar, Tooltip } from "@mantine/core";
 import React, { useEffect } from "react";
 
 interface ActiveUserInterface {
@@ -8,18 +8,19 @@ interface ActiveUserInterface {
 
 const ActiveUser: React.FC<ActiveUserInterface> = ({ name, isActive }) => {
   return (
-    <div className="flex flex-col items-center justify-center gap-1">
-      <img></img>
-      <Avatar
-        variant="filled"
-        size={100}
-        color="violet"
-        name={name}
-        className={`w-[100px] h-[100px] rounded-full bg-slate-400 border shadow-md shadow-slate-500  border-stone-300 transition-all duration-75  ${
-          isActive ? "outline-green-600 outline-4 outline " : "bg-slate-400 "
-        }`}
-        id="avatar"
-      ></Avatar>
+    <div className="flex flex-col items-center justify-center gap-1  px-2 py-4 border border-hidden hover:">
+      <Tooltip label={name} position="bottom" withArrow>
+        <Avatar
+          variant="filled"
+          size={100}
+          color="violet"
+          name={name}
+          className={`w-[100px] h-[100px] rounded-full bg-slate-400 border shadow-md shadow-slate-500  border-stone-300 transition-all duration-75  ${
+            isActive ? "outline-green-600 outline-4 outline " : "bg-slate-400 "
+          }`}
+          id="avatar"
+        ></Avatar>
+      </Tooltip>
     </div>
   );
 };
