4fc8c22316664455bcb8515c700b9cf3e690029c
Mateusz Duma <mateuszsuplice@gmail.com>
2024-12-24 00:34:22 +0100

    feat: enhance ReportBugMenu with animation and new input fields for improved user interaction

--- frontend/src/Components/ReportBugMenu/ReportBugMenu.tsx
+++ frontend/src/Components/ReportBugMenu/ReportBugMenu.tsx
@@ -1,9 +1,20 @@
+import { motion } from "motion/react";
 import React from "react";
 
 const ReportBugMneu = () => {
   return (
     <div className="fixed inset-0 z-20 flex items-center justify-center bg-gray-800 bg-opacity-75 backdrop-blur-md w-full h-full px-2 md:px-0 ">
-      <div className="xl:w-2/5 lg:w-3/5 md:w-3/5 sm:w-4/5 w-full flex items-center justify-center bg-white md:h-3/5 h-4/5 rounded-lg   "></div>
+      <motion.div
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1, transition: { duration: 0.3 } }}
+        exit={{ y: -50, opacity: 0, transition: { duration: 0.3 } }}
+        className="xl:w-2/5 lg:w-3/5 md:w-3/5 sm:w-4/5 w-full flex items-center bg-white md:h-3/5 h-4/5 rounded-lg p-3 flex-col   "
+      >
+        <label>test</label>
+        <input></input>
+        <label>test2</label>
+        <input></input>
+      </motion.div>
     </div>
   );
 };
