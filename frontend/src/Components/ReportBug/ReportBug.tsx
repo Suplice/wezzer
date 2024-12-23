44dd7edfb7bc95f52172105ebc55ff22f5a04579
Mateusz Duma <mateuszsuplice@gmail.com>
2024-12-23 23:35:20 +0100

    feat: update ReportBug component with new styling and layout

--- frontend/src/Components/ReportBug/ReportBug.tsx
+++ frontend/src/Components/ReportBug/ReportBug.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ReportBug: React.FC = () => {
-  return <div>Report Bug</div>;
+  return (
+    <div className="fixed bottom-12 right-12 w-12 h-12 bg-blue-500 rounded-lg hover:bg-blue-600 hover:cursor-pointer">
+      dada
+    </div>
+  );
 };
 
 export default ReportBug;
