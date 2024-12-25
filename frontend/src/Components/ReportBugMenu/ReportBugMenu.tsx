e698316d34008be5014d58637b09044e227a94a8
Mateusz Duma <mateuszsuplice@gmail.com>
2024-12-25 22:00:24 +0100

    feat: enhance ReportBugMenu form with improved styling and layout for better usability

--- frontend/src/Components/ReportBugMenu/ReportBugMenu.tsx
+++ frontend/src/Components/ReportBugMenu/ReportBugMenu.tsx
@@ -8,20 +8,25 @@ const ReportBugMneu = () => {
         initial={{ y: 50, opacity: 0 }}
         animate={{ y: 0, opacity: 1, transition: { duration: 0.3 } }}
         exit={{ y: -50, opacity: 0, transition: { duration: 0.3 } }}
-        className="xl:w-2/5 lg:w-3/5 md:w-3/5 sm:w-4/5 w-full flex items-center bg-white md:h-3/5 h-4/5 rounded-lg p-3 flex-col   "
+        className="xl:w-2/5 lg:w-3/5 md:w-3/5 sm:w-4/5 w-full flex items-center bg-white md:h-3/5 h-4/5 rounded-lg p-6 flex-col   "
       >
-        <form>
-          <div className="flex flex-col">
+        <form className="flex flex-col w-full h-full justify-between">
+          <div className="flex flex-col gap-2">
             <label>Title</label>
-            <input></input>
+            <input className="outline-none border rounded-md p-1"></input>
             <label>Description</label>
-            <input></input>
+            <input className="outline-none border rounded-md p-1"></input>
           </div>
-          <div className="flex justify-end flex-row">
-            <button type="submit" className="py-2 px-4 bg-blue-500">
+          <div className="flex justify-end flex-row gap-3 ">
+            <button
+              type="submit"
+              className="py-2 px-4 bg-blue-500 rounded-sm hover:bg-blue-600 transition-all duration-200 "
+            >
               Send
             </button>
-            <button className="py-2 px-4 bg-gray-500"> Cancel </button>
+            <button className="py-2 px-4 bg-gray-500 rounded-sm hover:bg-gray-600 transition-all duration-200">
+              Cancel
+            </button>
           </div>
         </form>
       </motion.div>
